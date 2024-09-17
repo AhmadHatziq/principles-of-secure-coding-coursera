72c0550a542215c886dc3d0d2bfaecd9fbbe7dc5
Hatziq <hatziq@u.nus.edu>
2024-09-17 23:03:18 +0800

    add skeleton files for fragile-code

---
+++ module-3/fragile-code/queue.h
@@ -0,0 +1,7 @@
+/* the queue structure*/
+typedef struct queue {
+    int *que; // Array of queue elements
+    int head; // Head index of the queue
+    int count;// Number of elements 
+    int size; // Max number of elements 
+} QUEUE ; 
